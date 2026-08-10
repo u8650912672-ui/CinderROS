b4b57b686c1c6af4b13b3a6ce3f330e660d3046b
John <u8650912672-ui@gmail.com>
2026-08-10 13:12:30 +0200

    start.asm

--- boot/bios/stage1/start.asm
+++ boot/bios/stage1/start.asm
@@ -12,13 +12,13 @@
 
     ;nr 2 set text mode IF THIS DONT WORK MANUALLY CHANGE IT TO mov ax, 0x0003 
     call set_80x50
-    ;nr3 obselete i will cahnge and erase it later 
+    ;nr3 boot msg
     mov si, msg_boot
     call print
-    ;nr 3 real load stage 2 from disk try lab first the fallback to CHS
+    ;nr 4 load stage 2 from disk try lab first the fallback to CHS
     mov si, disk_address_packet
     mov ah, 0x42
-    mod dl, [boot_drive]
+    mov dl, [boot_drive]
     int 0x13
     jnc .load_ok
 
@@ -31,11 +31,11 @@
     int 0x13
     jc error
 
-.load_ok
+.load_ok:
     ;stage2 loaded seccessfully at 0x0000:0x7E0
-    jmp 0x0000:ex7E00
+    jmp 0x0000:0x7E00
 
-    ;nr 4 error handeling
+    ;nr 5 error handeling
 error:
     mov si, msg_error
     call print
@@ -43,7 +43,7 @@ error:
     hlt
     jmp error ;in case of NMI or wakeup :p
 
-    ;nr 5 helper and i/o without o
+    ;nr 6 helper and i/o without o
 print:
     lodsb
     or al, al
@@ -54,8 +54,8 @@ print:
 .done:
     ret
 
-    ;nr 6set the vga 80x50
-set_80x50
+    ;nr 7 set the vga 80x50
+set_80x50:
     mov ax, 0x0003
     int 0x10
 
@@ -89,4 +89,38 @@ set_80x50
     out dx, al
 
     inc dx
-    in al,<
+    in al, dx
+    or al, 0x02
+
+    out dx, al
+
+    ;clear screen
+    mov ax, 0x0600
+    mov bh, 0x07
+    xor cx, cx
+
+    mov dx, 0x184F
+    int 0x10
+    ret
+
+    ;nr 8 data
+msg_boot db "stage1: Loading into stage2 standy...", 13, 10, 0
+msg_error db "AN ERROR HAS OCCURED: Error>could not load stage2.-1", 13, 10, 0
+
+boot_drive db 0x80 ;return and boot the default drive
+
+disk_address_packet:
+    db 0x10
+    db 0x00
+    dw 1
+    dw 0x7E00
+    dw 0x0000
+    dd 1
+    dd 0
+
+
+    ;nr 9 boot signature to make it bootable
+    times 510 - ($ - $$) db 0
+    dw 0xAA55
+
+
