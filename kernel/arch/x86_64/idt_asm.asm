ab180f2c358a6a59ecf5f2ffd914b70bd4fd1741
John <u8650912672-ui@gmail.com>
2026-08-14 22:33:04 +0200

    fixed the last fix that didnt fix the fix that i wanted to fix?

--- kernel/arch/x86_64/idt_asm.asm
+++ kernel/arch/x86_64/idt_asm.asm
@@ -3,6 +3,25 @@ section .text
 extern irq_handler
 global isr_stub
 isr_stub:
+    push rax
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
     cli
     call irq_handler
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rax
     iretq
+    ;thanks jonathan the genius asm and keyboard helper
